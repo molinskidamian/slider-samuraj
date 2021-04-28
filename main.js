141717cfebcd8336dd5630ba1e745709f22a9a7f
Damian Molinski <tedeer@gmail.com>
2021-04-28 22:06:42 +0200

    Finished project

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const slideList = [{
 
 const img = document.querySelector('img.slider');
 const h1 = document.querySelector('h1');
+const dots = document.querySelectorAll('span');
 
 const time = 5000;
 let active = 1;
@@ -22,10 +23,15 @@ const changeSlide = () => {
   if(active === slideList.length){
     active = 0;
   }
-  
+
   img.removeAttribute('img');
   img.setAttribute("src", slideList[active].img);
   h1.textContent = slideList[active].text;
+
+  dots.forEach(el => {
+    el.classList.remove('active');
+  })
+  dots[active].classList.add('active');
 
   console.log(active);
   active++;
